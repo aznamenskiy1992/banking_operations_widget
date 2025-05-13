93f72b8185016ccbe49143e214c85d0fc6f801f0
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-13 13:42:31 +0300

    Добавлена функция для конвертации даты из строки в формат DD.MM.YYYY

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from masks import get_mask_account, get_mask_card_number
 
 
@@ -9,3 +11,14 @@ def mask_account_card(card_or_account_number: str) -> str:
     else:
         masked_data: str = get_mask_card_number(int(card_or_account_number_split[-1]))
     return card_or_account_number_split[0] + " " + masked_data
+
+
+def get_date(date_: str) -> str:
+    """Функция, которая преобразовывает дату из строки в формат DD.MM.YYYY"""
+    date_split: list[str] = date_.split("T")
+    formatted_date_by_y_m_d: str = str(datetime.strptime(date_split[0], "%Y-%m-%d").date())
+    formatted_date_by_y_m_d_split: list[str] = formatted_date_by_y_m_d.split("-")
+    formatted_date_by_d_m_y: str = ".".join(
+        [formatted_date_by_y_m_d_split[-1], formatted_date_by_y_m_d_split[1], formatted_date_by_y_m_d_split[0]]
+    )
+    return formatted_date_by_d_m_y
