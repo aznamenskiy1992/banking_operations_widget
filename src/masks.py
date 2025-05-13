dcb912c713b60d2a2d4bcd30eab620b006d99d2e
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-13 13:05:32 +0300

    Добавлена функция маскирования номера счёта

--- src/masks.py
+++ src/masks.py
@@ -8,3 +8,9 @@ def get_mask_card_number(card_number: int) -> str:
     ]
     mask_card_number: str = " ".join(dividing_card_number_by_blocks)
     return mask_card_number
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция, которая маскирует номер счёта"""
+    mask_account_number: str = "*" * 2 + str(account_number)[16:]
+    return mask_account_number
