2249f8ab8ce51fe4b41ed7a9d1d5392497a56426
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-05-22 19:41:02 +0300

    создал функцию, соритрующую словари в списке по дате

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,16 @@ def filter_by_state(
     Получает список словарей и возвращает список словарей, у которых ключ state соответствует переданному значению
     """
     return list(filter(lambda data: data["state"] == state, source_data))
+
+
+def sort_by_date(
+    source_data: list[dict[str, Union[str, int]]], sort: str = "True"
+) -> list[dict[str, Union[str, int]]]:
+    """
+    Сортирует словари по дате (значению ключа date)
+    """
+    if sort.lower() == "true":
+        reverse = True
+    else:
+        reverse = False
+    return sorted(source_data, key=lambda data: data["date"], reverse=reverse)
