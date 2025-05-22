e818149a0b222397acb8294afc664778018a4ee7
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-05-22 08:46:15 +0300

    fixed masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция, которая маскирует номер карты"""
     dividing_card_number_by_blocks: list[str] = [
-        str(card_number)[:5],
+        str(card_number)[:4],
         str(card_number)[4:6] + "*" * 2,
         "*" * 4,
         str(card_number)[12:],
