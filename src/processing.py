7269c9f9b682da13946b4f68a2e21fcb5b38c8a3
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-05-22 19:43:09 +0300

    исправлен docstring для функции filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -4,9 +4,7 @@ from typing import Union
 def filter_by_state(
     source_data: list[dict[str, Union[str, int]]], state: str = "EXECUTED"
 ) -> list[dict[str, Union[str, int]]]:
-    """
-    Получает список словарей и возвращает список словарей, у которых ключ state соответствует переданному значению
-    """
+    """Функция, которая фильтрует список словарей по ключу state"""
     return list(filter(lambda data: data["state"] == state, source_data))
 
 
