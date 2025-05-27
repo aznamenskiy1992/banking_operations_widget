15c9c71023c583fc8525a89d1906079beffb8530
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-05-27 21:58:22 +0300

    fixed функции, тестирующей get_mask_card_number модуля masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,5 +6,5 @@ from src.masks import get_mask_card_number
 @pytest.mark.parametrize("card_number, mask_number", [
     (5543812355785520, "5543 81** **** 5520")
 ])
-def input_standard_card_number(card_number, mask_number): # 16 цифр
+def test_input_standard_card_number(card_number, mask_number): # 16 цифр
     assert get_mask_card_number(card_number) == mask_number
