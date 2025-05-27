c33f32a3c0d9d603ccf0f14c270e939d7bed58bc
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-05-27 22:12:13 +0300

    создан тест для функции get_mask_card_number модуля masks. проверяем маскирование нестандартных номеров карты (13,18,19)

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,7 +4,10 @@ from src.masks import get_mask_card_number
 
 
 @pytest.mark.parametrize("card_number, mask_number", [
-    (5543812355785520, "5543 81** **** 5520")
+    (5543812355785520, "5543 81** **** 5520"), # 16 цифр
+    (5543812355785, "5543 81* *** 785"), # 13 цифр
+    (554381235578552014, "5543 81** **** **2014"), # 18 цифр
+    (5543812355785520439, "5543 81** **** **** 439"), # 19 цифр
 ])
-def test_input_standard_card_number(card_number, mask_number): # 16 цифр
+def test_input_standard_card_number(card_number, mask_number):
     assert get_mask_card_number(card_number) == mask_number
