4f77d9270b7c00aa2fc7b080ee7c4fd33694ad54
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-28 20:46:39 +0300

    создан тест для кейса по вводу номера карты строкой, где все символы цифры для функции get_mask_card_number модуля masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -20,3 +20,13 @@ def test_none_card_number(none_card_and_account_number):
 def test_none_standard_card_number(none_standard_card_and_account_number):
     assert get_mask_card_number(123) == none_standard_card_and_account_number
     assert get_mask_card_number(15234820356820125369) == none_standard_card_and_account_number
+
+
+@pytest.mark.parametrize("card_number, mask_number", [
+    ("5543812355785520", "5543 81** **** 5520"), # 16 цифр
+    ("5543812355785", "5543 81* *** 785"), # 13 цифр
+    ("554381235578552014", "5543 81** **** **2014"), # 18 цифр
+    ("5543812355785520439", "5543 81** **** **** 439"), # 19 цифр
+])
+def test_card_number_str_all_symbols_int(card_number, mask_number):
+    assert get_mask_card_number(card_number) == mask_number
