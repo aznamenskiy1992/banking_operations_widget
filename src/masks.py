876e5bd114cdd57ac892af77ab4519c6443b9d30
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-28 16:03:35 +0300

    обработан кейс по маскированию 13-ти значного номера карты  для функции get_mask_card_number модуля masks

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,19 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция, которая маскирует номер карты"""
-    dividing_card_number_by_blocks: list[str] = [
-        str(card_number)[:4],
-        str(card_number)[4:6] + "*" * 2,
-        "*" * 4,
-        str(card_number)[12:],
-    ]
+    if len(str(card_number)) == 16:
+        dividing_card_number_by_blocks: list[str] = [
+            str(card_number)[:4],
+            str(card_number)[4:6] + "*" * 2,
+            "*" * 4,
+            str(card_number)[12:],
+        ]
+    elif len(str(card_number)) == 13:
+        dividing_card_number_by_blocks: list[str] = [
+            str(card_number)[:4],
+            str(card_number)[4:6] + "*",
+            "*" * 3,
+            str(card_number)[-3:],
+        ]
     mask_card_number: str = " ".join(dividing_card_number_by_blocks)
     return mask_card_number
 
