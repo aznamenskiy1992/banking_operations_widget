7aba4aa1303aa39d9c215d258f3b85f4e00e3dac
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 21:09:31 +0300

    обработан кейс, где вводится пустое значение вместо списка словарей. Для функции filter_by_state модуля processing

--- src/processing.py
+++ src/processing.py
@@ -5,6 +5,9 @@ def filter_by_state(
     source_data: list[dict[str, Union[str, int]]], state: str = "EXECUTED"
 ) -> Union[list[dict[str, Union[str, int]]], str]:
     """Функция, которая фильтрует список словарей по ключу state"""
+    if source_data is None:
+        return "Не указан список словарей"
+
     try:
         filtered_data = list(filter(lambda data: data["state"] == state, source_data))
     except:
