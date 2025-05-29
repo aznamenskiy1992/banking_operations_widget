3493acfe8e302819c3416706c4b27a36cc621220
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 14:09:04 +0300

    обработан кейс ввода различных номеров счетов. Для функции mask_account_card модуля widget

--- src/widget.py
+++ src/widget.py
@@ -7,7 +7,7 @@ def mask_account_card(card_or_account_number: str) -> str:
     """Функция, которая маскирует номер карты и счёта"""
     card_or_account_number_split: list[str] = card_or_account_number.split()
 
-    if "счет" in card_or_account_number.lower():
+    if "счет" in card_or_account_number.lower() or "счёт" in card_or_account_number.lower():
         masked_data: str = get_mask_account(int(card_or_account_number_split[-1]))
     else:
         masked_data: str = get_mask_card_number(int(card_or_account_number_split[-1]))
