a010a97f6ce9c7999fa2935fa51cbeb0c214cfa0
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 07:50:32 +0300

    создан тест для кейса ввода номера карты строкой, где часть символов не цифры. Для функции get_mask_card_number модуля masks

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,8 @@ def none_card_and_account_number():
 @pytest.fixture
 def none_standard_card_and_account_number():
     return "Указан некорректный номер карты или счёта. Проверьте количество цифр"
+
+
+@pytest.fixture
+def card_and_account_number_str_symbols_not_int():
+    return "Номер карты или счёта должен состоять только из цифр"
