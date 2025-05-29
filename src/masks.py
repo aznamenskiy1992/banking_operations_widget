fd95fa14c948cd3ffb0f55518af2d3f044899975
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 09:12:04 +0300

    сделал рефакторинг в функции get_mask_card_number модуля masks

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,23 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция, которая маскирует номер карты"""
+
+    # Входные данные пустые
     if card_number is None:
         return "Не указан номер карты или счёта"
 
-    if isinstance(card_number, int):
+    if isinstance(card_number, int):  # Входные данные в типе int
         card_number_str = str(card_number)
-    elif isinstance(card_number, str):
+    elif isinstance(card_number, str):  # Входные данные в типе str
         if not card_number.isdigit():
-            return "Номер карты или счёта должен состоять только из цифр"
+            return "Номер карты или счёта должен состоять только из цифр"  # Строка содержит не только цифры
         else:
             card_number_str = card_number
     else:
-        return "Номер карты или счёта должен быть целым числом"
+        return "Номер карты или счёта должен быть целым числом"  # Входные данные не str или int
 
-    len_card_number = len(str(card_number))
+    len_card_number = len(card_number_str)
 
+    # Словарь с данными для маскирования
     mask_patterns = {
         16: {
             "masking": (6, 12),
@@ -35,37 +38,42 @@ def get_mask_card_number(card_number: int) -> str:
     }
 
     if len_card_number in list(mask_patterns.keys()):
+        # Позиции для среза строки
         masking_position_start: int = mask_patterns[len_card_number]["masking"][0]
         masking_position_end: int = mask_patterns[len_card_number]["masking"][1]
 
+        # Кол-во используемых звёздочек для маскирования
         masking_count: int = masking_position_end - masking_position_start
 
+        # Маскируем строку
         mask_card_number: str = (
             card_number_str[:masking_position_start] + "*" * masking_count + card_number_str[masking_position_end:]
         )
 
+        # Позиции для разделения строки
         dividing_start_position: int = 0
         dividing_end_position: int = mask_patterns[len_card_number]["sep"][0]
+
         dividing_card_number_by_blocks: list[str] = []
 
         for index in range(len(mask_patterns[len_card_number]["sep"])):
             if index == 0:
-                dividing_card_number_by_blocks.append(mask_card_number[:dividing_end_position])
+                dividing_card_number_by_blocks.append(mask_card_number[:dividing_end_position])  # Первая часть строки
             else:
                 dividing_start_position += mask_patterns[len_card_number]["sep"][index - 1]
                 dividing_end_position += mask_patterns[len_card_number]["sep"][index]
                 if index == len(mask_patterns[len_card_number]["sep"]) - 1:
-                    dividing_card_number_by_blocks.append(mask_card_number[dividing_start_position:])
+                    dividing_card_number_by_blocks.append(
+                        mask_card_number[dividing_start_position:]
+                    )  # Окончание строки
                 else:
                     dividing_card_number_by_blocks.append(
                         mask_card_number[dividing_start_position:dividing_end_position]
                     )
     else:
-        return "Указан некорректный номер карты или счёта. Проверьте количество цифр"
+        return "Указан некорректный номер карты или счёта. Проверьте количество цифр"  # Входные данные с некоректным кол-вом цифр
 
-    mask_card_number: str = " ".join(dividing_card_number_by_blocks)
-
-    return mask_card_number
+    return " ".join(dividing_card_number_by_blocks)
 
 
 def get_mask_account(account_number: int) -> str:
