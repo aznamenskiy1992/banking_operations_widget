a2fba6820c83524cfaba9f59637bb24f2ac2a56e
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 21:20:01 +0300

    создан тест для кейса сортировки списка словарей по ключу date. Для функции sort_by_date модуля processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.mark.parametrize("src_dicts, state, filter_dicts", [
@@ -72,3 +72,35 @@ def test_state_key_not_in_dicts(src_dicts, state, notice):
 ])
 def test_none_list_dicts(src_dicts, notice):
     assert filter_by_state(src_dicts) == notice
+
+
+@pytest.mark.parametrize("src_dicts, reverse_, sort_dicts", [
+    ([
+         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+     ],
+     "True",
+     [
+         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+     ]),
+    ([
+         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+     ],
+     "False",
+     [
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     ]),
+    ])
+def test_sort_dict_by_date_key(src_dicts, reverse_, sort_dicts):
+    assert sort_by_date(src_dicts, reverse_) == sort_dicts
