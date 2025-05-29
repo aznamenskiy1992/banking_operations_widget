eb2d31b484394e9d1dbf0a6126a3bfe60cac6243
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 20:57:10 +0300

    создан тест для кейса отстутствия ключа state в списке словарей. Для функции filter_by_state модуля processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -43,3 +43,25 @@ def test_filter_dict_by_state_key(src_dicts, state, filter_dicts):
 ])
 def test_note_have_value_state_in_dicts(src_dicts, state, notice):
     assert filter_by_state(src_dicts, state) == notice
+
+
+@pytest.mark.parametrize("src_dicts, state, notice", [
+    ([
+         {'id': 41428829, 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}
+     ],
+     "EXECUTED",
+     "В словарях нет ключа 'state'"),
+    ([
+         {'id': 41428829, 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'date': '2018-10-14T08:21:33.419441'}
+     ],
+     "CANCELED",
+     "В словарях нет ключа 'state'"),
+])
+def test_state_key_not_in_dicts(src_dicts, state, notice):
+    assert filter_by_state(src_dicts, state) == notice
