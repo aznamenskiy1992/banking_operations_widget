44b27c7f26a203e6ec7b4933cbe0a98c6555f5f9
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 13:26:13 +0300

    провёл рефакторинг кода функции mask_account_card модуля widget

--- src/widget.py
+++ src/widget.py
@@ -6,10 +6,12 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(card_or_account_number: str) -> str:
     """Функция, которая маскирует номер карты и счёта"""
     card_or_account_number_split: list[str] = card_or_account_number.split()
+
     if "счет" in card_or_account_number.lower():
         masked_data: str = get_mask_account(int(card_or_account_number_split[-1]))
     else:
         masked_data: str = get_mask_card_number(int(card_or_account_number_split[-1]))
+
     if len(card_or_account_number_split) == 2:
         return card_or_account_number_split[0] + " " + masked_data
     else:
