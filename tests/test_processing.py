a2a854c4ad56e02f1cd84e046ba7d7a189e402aa
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 20:44:03 +0300

    создан тест для кейса ввода значения state, которого нет в словарях. Для функции filter_by_state модуля processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -29,3 +29,17 @@ from src.processing import filter_by_state
 ])
 def test_filter_dict_by_state_key(src_dicts, state, filter_dicts):
     assert filter_by_state(src_dicts, state) == filter_dicts
+
+
+@pytest.mark.parametrize("src_dicts, state, notice", [
+    ([
+         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+     ],
+     "NEXT",
+     "Нет словарей со значением state"),
+])
+def note_have_value_state_in_dicts(src_dicts, state, notice):
+    assert filter_by_state(src_dicts, state) == notice
