17f184e00d62d1a609bbbbb428d52a5b6d8f2838
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 13:15:18 +0300

    создан тест для кейса ввода номера карты 13, 18, 19 символов. Для функции mask_account_card модуля widget

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -9,3 +9,12 @@ from src.widget import mask_account_card
 ])
 def test_definition_account_and_card_number(number, mask_number):
     assert mask_account_card(number) == mask_number
+
+
+@pytest.mark.parametrize("card_number, mask_number", [
+    ("Visa Classic 5543812355785", "Visa Classic 5543 81* *** 785"),  # 13 цифр
+    ("Maestro 554381235578552014", "Maestro 5543 81** **** **2014"),  # 18 цифр
+    ("Visa Platinum 5543812355785520439", "Visa Platinum 5543 81** **** **** 439"),  # 19 цифр
+])
+def test_definition_other_card_number(card_number, mask_number):
+    assert mask_account_card(card_number) == mask_number
