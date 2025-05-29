522475d25e9ff9431d7a81150a19934f5f4f089c
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-05-29 13:25:11 +0300

    обработан кейс ввода различных названий счетов

--- src/widget.py
+++ src/widget.py
@@ -6,7 +6,7 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(card_or_account_number: str) -> str:
     """Функция, которая маскирует номер карты и счёта"""
     card_or_account_number_split: list[str] = card_or_account_number.split()
-    if "Счет" in card_or_account_number:
+    if "счет" in card_or_account_number.lower():
         masked_data: str = get_mask_account(int(card_or_account_number_split[-1]))
     else:
         masked_data: str = get_mask_card_number(int(card_or_account_number_split[-1]))
