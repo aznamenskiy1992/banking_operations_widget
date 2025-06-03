5820dc79c5d022db1ec7671953e2ea9914f1f1f0
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-03 14:00:14 +0300

    исправил тест, где на внутри списка транзакции находятся не в словарях. Для функции filter_by_currency модуля generators

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -96,7 +96,8 @@ def test_empty_transactions_list_for_filter_by_currancy_and_transaction_descript
 
 def test_in_transactions_list_not_dict_for_filter_by_currancy_and_transaction_descriptions(in_transactions_list_not_dict_for_filter_by_currancy_and_transaction_descriptions):
     """Тестирует обработку кейса, когда на вход подаётся список с транзакциями не в словарях"""
-    assert filter_by_currency([{(939719570, 9824.07), (142264268, "EXECUTED")}], "USD") == in_transactions_list_not_dict_for_filter_by_currancy_and_transaction_descriptions
+    generator = filter_by_currency([{(939719570, 9824.07), (142264268, "EXECUTED")}], "USD")
+    assert next(generator) == in_transactions_list_not_dict_for_filter_by_currancy_and_transaction_descriptions
 
 
 def test_filter_by_currency():
