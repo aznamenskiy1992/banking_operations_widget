1a2de1a57ee7dbcb3d25873d082f1363fbf3e925
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-03 13:52:47 +0300

    исправил тест для кейса ввода None вместо списка транзакций. Для функции filter_by_currency модуля generators

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -84,7 +84,8 @@ example_input_transactions_for_for_filter_by_currancy_and_transaction_descriptio
 
 def test_none_list_for_filter_by_currency(none_transactions_list_for_filter_by_currancy_and_transaction_descriptions):
     """Тестирует обработку None в качестве списка словарей."""
-    assert filter_by_currency(None, "USD") == none_transactions_list_for_filter_by_currancy_and_transaction_descriptions
+    generator = filter_by_currency(None, "USD")
+    assert next(generator) == none_transactions_list_for_filter_by_currancy_and_transaction_descriptions
 
 
 def test_empty_transactions_list_for_filter_by_currancy_and_transaction_descriptions(empty_transactions_list_for_filter_by_currancy_and_transaction_descriptions):
