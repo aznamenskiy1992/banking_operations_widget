9198efec48ee3dfa15a0a573b58b10a5f0e3facf
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-10 15:50:52 +0300

    исправил обработку кейса, где вместо номера счёта подаётся None. Для функции get_mask_account модуля masks

--- src/masks.py
+++ src/masks.py
@@ -112,7 +112,7 @@ def get_mask_account(account_number: int) -> str:
     """
     # Проверка на None
     if account_number is None:
-        return "Не указан номер карты или счёта"
+        raise ValueError("Не указан номер карты или счёта")
 
     # Преобразование номера счёта в строку с проверкой типа
     if isinstance(account_number, int):
