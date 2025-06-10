d2bf2c0d1db459184170929569f1adafc8b45228
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-10 16:08:55 +0300

    исправил тест для кейса, где в списке словарей нет ключа state. Для функции filter_by_state модуля processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -88,7 +88,7 @@ def test_state_key_not_in_dicts(src_dicts, state, notice):
     """Тестирует обработку случая отсутствия ключа 'state' в словарях."""
     with pytest.raises(KeyError) as exc_info:
         filter_by_state(src_dicts, state)
-    assert str(exc_info.value) == "В словарях нет ключа 'state'"
+    assert str(exc_info.value) == notice
 
 
 def test_none_list_dicts():
