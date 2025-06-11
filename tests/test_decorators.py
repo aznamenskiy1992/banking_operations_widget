0b985d6b419c892254d2439b850f2a0e238d2294
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-11 20:37:52 +0300

    создал тест для кейса декоратора log, где в консоль выводится информация об успешной операции. Для функции filter_by_state

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -45,6 +45,25 @@ def test_log_success_operation_in_file_for_filter_by_state_with_log_decorator(ex
         assert content[-1].strip() == "filter_by_state ok"
 
 
+def test_log_success_operation_in_console_for_filter_by_state_with_log_decorator(capsys):
+    """Тестирует выполнение успешной операции. Логи в консоль"""
+    @log()
+    def filter_by_state(source_data, state = "EXECUTED"):
+        filtered_data = list(filter(lambda data: data["state"] == state, source_data))
+        return filtered_data
+
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+    captured = capsys.readouterr()
+    assert captured.out == "filter_by_state ok\n"
+
+
 def test_log_error_operation_in_file_for_get_mask_card_number_with_log_decorator(example_input_none_for_get_mask_card_number):
     """Тестирует логирование ошибок при выполнении операции. Логи в файл"""
     @log("mylog.txt")
