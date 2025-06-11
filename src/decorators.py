0d0f0c3a3ec599a2ee49a20e8a7dabf7aa4ddcd9
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-11 21:27:54 +0300

    провёл рефакторинг кода в декораторе log

--- src/decorators.py
+++ src/decorators.py
@@ -1,10 +1,11 @@
-from typing import Union
-from functools import wraps
 import os
+from functools import wraps
+from typing import Union
 
 
-def log(filename = None):
+def log(filename=None):
     """Декоратор, который логирует информацию об операциях в файл mylog.txt или в консоль"""
+
     def log_inner(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -12,23 +13,37 @@ def log(filename = None):
                 raise ValueError("Неверное название файла логов. Должно быть 'mylog.txt'")
 
             try:
-                result = func(*args, **kwargs)
+                func(*args, **kwargs)
             except Exception as exc_info:
                 inputs = args[0] if len(args) == 1 and not kwargs else (args if args else kwargs)
 
                 if filename is not None:
-                    with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "logs", filename), "a", encoding="utf-8") as file:
+                    with open(
+                        os.path.join(
+                            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "logs", filename
+                        ),
+                        "a",
+                        encoding="utf-8",
+                    ) as file:
                         file.write(f"{func.__name__} error: {str(exc_info)}. Inputs: {inputs}\n")
 
                 else:
                     print(f"{func.__name__} error: {str(exc_info)}. Inputs: {inputs}")
             else:
                 if filename is not None:
-                    with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "logs", filename), "a", encoding="utf-8") as file:
+                    with open(
+                        os.path.join(
+                            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "logs", filename
+                        ),
+                        "a",
+                        encoding="utf-8",
+                    ) as file:
                         file.write(f"{func.__name__} ok\n")
 
                 else:
                     print(f"{func.__name__} ok")
             return None
+
         return wrapper
+
     return log_inner
