e282a41ea4d517a6e1458e614e9f6b890d38e6e0
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-16 19:36:19 +0300

    испрвил тест для кейса, когда в файле с банковскими операциями данные содержутся не в списке. Для функции get_transactions модуля utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -75,54 +75,26 @@ def test_incorrect_path_to_operations_json():
 
 def test_not_list_in_operations_json():
     """Тестирует обработку кейса, когда в файле с банковскими операциями данные содержутся не в списке"""
-    fake_json_data = (
-        {
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {
-                "amount": "31957.58",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589"
-        },
-        {
-            "id": 41428829,
-            "state": "EXECUTED",
-            "date": "2019-07-03T18:35:29.512364",
-            "operationAmount": {
-                "amount": "8221.37",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560"
-        },
-        {
-            "id": 667307132,
-            "state": "EXECUTED",
-            "date": "2019-07-13T18:51:29.313309",
-            "operationAmount": {
-                "amount": "97853.86",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "Maestro 1308795367077170",
-            "to": "Счет 96527012349577388612"
-        }
+    fake_json_data = []
+    fake_json_str = json.dumps(
+        (
+            {
+                "id": 667307132,
+                "state": "EXECUTED",
+                "date": "2019-07-13T18:51:29.313309",
+                "operationAmount": {
+                    "amount": "97853.86",
+                    "currency": {
+                        "name": "руб.",
+                        "code": "RUB"
+                    }
+                },
+                "description": "Перевод с карты на счет",
+                "from": "Maestro 1308795367077170",
+                "to": "Счет 96527012349577388612"
+            }
+        )
     )
-    fake_json_str = json.dumps(fake_json_data)
 
     with patch("builtins.open", mock_open(read_data=fake_json_str)) as mocked_open:
         result = get_transactions("operations.json")
