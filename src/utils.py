7f501712d0aaf75dabcce3873d81dd68d83a1bba
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-16 19:01:46 +0300

    обработал кейс, где возвращается список боанковских операций из operations.json. Для функции get_transactions модуля utils

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,9 @@
+import json
+
+
 def get_transactions(path_to_operations_file: str) -> list[dict[str, int]]:
-    pass
+    """Функция возвращает список банковских операций из operations.json"""
+    with open(path_to_operations_file, "r", encoding="utf-8") as f:
+        operations: list[dict[str, int]] = json.load(f)
+
+    return operations
