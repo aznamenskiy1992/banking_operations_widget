126d4c23db9f981d801fa8c8594a6dc2c99dad1e
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-16 19:15:10 +0300

    обработал кейс, где на вход подайётся некорректный путь до файла с банковскими операциями. Для функции get_transactions модуля utils

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,11 @@ import json
 
 def get_transactions(path_to_operations_file: str) -> list[dict[str, int]]:
     """Функция возвращает список банковских операций из operations.json"""
-    with open(path_to_operations_file, "r", encoding="utf-8") as f:
-        operations: list[dict[str, int]] = json.load(f)
+    try:
+        with open(path_to_operations_file, "r", encoding="utf-8") as f:
+            operations: list[dict[str, int]] = json.load(f)
+    except FileNotFoundError:
+        print("Не найден файл по указанному пути")
+        return []
 
     return operations
