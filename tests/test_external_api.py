4020fb842721fc5c0727a281125afcc0fa443fb1
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-06-17 21:29:03 +0300

    создал тест для кейса, где обрабатываю HTTP ошибки. Для функции convert_currency модуля external_api

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -2,6 +2,7 @@ from json import JSONDecodeError
 from unittest.mock import patch, Mock
 
 import pytest
+import requests
 
 from src.external_api import convert_currency
 
@@ -25,6 +26,28 @@ def test_get_success_response_for_convert_currency(mock_get):
     }
 
     assert convert_currency("USD", 10.2) == 799.603939
+
+    mock_get.assert_called_once_with(
+        "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=10.2",
+        headers={"apikey": "my_API_key"},
+        data={}
+    )
+
+
+@patch("requests.get")
+def test_http_error_for_convert_currency(mock_get):
+    """Тестирует обработку HTTP Error (400-599 статусы)"""
+    mock_get.return_value.status_code.return_value = 400
+    mock_get.return_value.json.return_value = {
+        "error": {
+            "code": "invalid_conversion_amount",
+            "message": "You have not specified an amount to be converted. [Example: amount=5]"
+        }
+    }
+
+    with pytest.raises(requests.HTTPError) as exc_info:
+        convert_currency("USD", 10.2)
+    assert str(exc_info.value) == "You have not specified an amount to be converted. [Example: amount=5]"
 
     mock_get.assert_called_once_with(
         "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=10.2",
