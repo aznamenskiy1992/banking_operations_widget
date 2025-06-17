b776e0a25d99de2d4c3c894049d1619e23d946dc
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-06-17 21:01:36 +0300

    исправил анотацию типов для функции convert_currency модуля external_api

--- src/external_api.py
+++ src/external_api.py
@@ -3,7 +3,7 @@ from typing import Union
 import requests
 
 
-def convert_currency(from_: str, amount: float) -> tuple[int, dict[str, Union[str, int, float, bool]]]:
+def convert_currency(from_: str, amount: float) -> float:
   """Функция конвертирует иностранную валюту в рубли по Exchange Rates Data API и возвращает ответ"""
   url: str = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={from_}&amount={amount}"
 
