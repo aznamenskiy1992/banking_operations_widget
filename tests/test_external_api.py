d529c1312ad1b7694f0e323d26a28669a52753aa
artem znamenskiy <aznamenskiy1992@gmail.com>
2025-06-17 17:30:43 +0300

    исправил тест кейса, где проверяю успешный ответ от API конвертации валют. Для функции convert_currency модуля external_api

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,4 +1,7 @@
-from unittest.mock import patch
+from json import JSONDecodeError
+from unittest.mock import patch, MagicMock
+
+import pytest
 
 from src.external_api import convert_currency
 
@@ -6,7 +9,7 @@ from src.external_api import convert_currency
 @patch("requests.get")
 def test_get_success_response_for_convert_currency(mock_get):
     """Тестирует успешный ответ от API конвертации валют"""
-    response_data = {
+    response_data = """"{
         "success": True,
         "query": {
             "from": "USD",
@@ -19,7 +22,7 @@ def test_get_success_response_for_convert_currency(mock_get):
         },
         "date": "2025-06-17",
         "result": 799.603939
-    }
+    }"""
 
     mock_get.return_value.json.return_value = response_data
     assert convert_currency("USD", 10.2) == response_data
