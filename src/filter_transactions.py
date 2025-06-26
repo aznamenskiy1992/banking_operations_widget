52a952c55cbe97dfc9de0dd8b1983f7f776d0f8b
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-26 12:21:06 +0300

    обработал кейс по возврату отфильтрованного списка словарей с операциями. Для функции process_bank_search модуля filter_transactions

--- src/filter_transactions.py
+++ src/filter_transactions.py
@@ -1,2 +1,6 @@
+import re
+
+
 def process_bank_search(data:list[dict], search:str) -> list[dict]:
-    pass
+    """Функция возвращает отфильтрованный список словарей с операциями по ключу description"""
+    return [e for i, e in enumerate(data) if re.search(rf"^{search}|.{search}.|{search}$", data[i]["description"], flags=re.IGNORECASE)]
