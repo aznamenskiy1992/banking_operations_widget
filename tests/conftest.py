d470a935cd8bceaf30c9f14fdaf3e6dd41fdf78b
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-27 15:58:33 +0300

    создал тест кейса по выводу отфильтрованных операций в консоль. Для функции main модуля main

--- tests/conftest.py
+++ tests/conftest.py
@@ -183,3 +183,84 @@ def transactions_from_files():
 def operation_categories():
     """Фикстура возвращает список категорий операций"""
     return ["Перевод организации", "Перевод со счета на счет", "Перевод с карты на карту"]
+
+
+@pytest.fixture
+def operations_for_main():
+    """Фикстура возвращает список операций для теста всей программы"""
+    return [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+                "amount": "48223.05",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
---
+++ tests/test_main.py
@@ -0,0 +1,61 @@
+import pytest
+from unittest.mock import patch
+
+from main import main
+
+
+@patch("builtins.input")
+@patch(main.get_transactions_from_xlsx)
+@patch(main.get_transactions_from_csv)
+@patch(main.get_transactions)
+@pytest.mark.parametrize(
+    "input_values, filtered_operations",
+    [
+        # Без сортировки по дате
+        # Без вывода только RUB транзакций
+        # Без фильтрации по слову в описании
+        (
+            {
+                "Пункт меню": 1,
+                "Статус для фильтрации": "Executed",
+                "Сортировка по дате": "нет",
+                "Вывод только руб": "нет",
+                "Фильтрация по слову": "нет",
+            },
+            """Всего банковских операций в выборке: 4
+            
+            26.08.2019 Перевод организации
+            Maestro 1596 83** **** 5199 -> Счет **9589
+            Сумма: 31957.58 руб.
+            
+            30.06.2018 Перевод организации
+            Счет **6952 -> Счет **6702
+            Сумма: 9824.07 USD
+            
+            23.03.2018 Открытие вклада
+            Счет **2431
+            Сумма: 48223.05 руб.
+            
+            19.08.2018 Перевод с карты на карту
+            Visa Classic 6831 98** **** 7658 -> Visa Platinum 8990 92** **** 5229
+            Сумма: 56883.54 USD
+            """
+        )
+    ]
+)
+def test_print_filtered_operation(mock_json, mock_csv, mock_xlsx, mock_input, input_values, filtered_operations, operations_for_main, capsys):
+    """Тестирует вывод отфильтрованных операций в консоль"""
+
+    # Значения из файлов
+    mock_json.return_value = operations_for_main
+    mock_csv.return_value = operations_for_main
+    mock_xlsx.return_value = operations_for_main
+
+    # Значения для input
+    input_values_list = [value for value in input_values.values()]
+    mock_input.side_effect = input_values_list
+
+    main()
+
+    captured = capsys.readouterr()
+    assert captured.out == filtered_operations
